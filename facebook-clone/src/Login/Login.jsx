eb463eb28d3a57d7a76f7b60821db2543b051556
dedayorr <dedayo16@gmail.com>
2023-04-03 22:34:05 +0100

    login

--- facebook-clone/src/Login/Login.jsx
+++ facebook-clone/src/Login/Login.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import "./Login.css"
 
 function Login() {
   return (
-    <div>Login</div>
+   <>
+    <form className='boxShadow w-[33%] h-[450px] mx-auto mt-[10%] rounded-lg'>
+      <p className='text-2xl text-center '>Log in to facebook</p>
+    </form>
+    </>
   )
 }
 
