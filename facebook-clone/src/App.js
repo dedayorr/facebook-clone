19b1be4caf004a092d752c868c9bfe5832567f1a
dedayorr <dedayo16@gmail.com>
2023-04-04 22:55:31 +0100

    updated

--- facebook-clone/src/App.js
+++ facebook-clone/src/App.js
@@ -1,13 +1,21 @@
-import './App.css';
+import "./App.css";
+import LeftSideBar from "./LeftSideBar/LeftSideBar";
 // import LeftSideBar from './LeftSideBar/LeftSideBar';
-import Login from './Login/Login';
-import Register from './Register/Register';
+import Login from "./Login/Login";
+import Register from "./Register/Register";
+import RightSideBar from "./RightSideBar/RightSideBar";
+import TopBar from "./TopBar/TopBar";
 
 function App() {
   return (
     <div className="App">
-      <Login/>
-      <Register />
+      {/* <Login/>
+      <Register /> */}
+      <TopBar />
+      <div className="flex justify-between">
+        <RightSideBar />
+        <LeftSideBar />
+      </div>
     </div>
   );
 }
